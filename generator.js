b18a3b7b0137d2ca9e1bab764ef1d0195b786913
Puneeth076 <95916647+Puneeth076@users.noreply.github.com>
2024-03-15 19:53:32 +0530

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,35 @@
+"use strict";
+
+process.title = "Ethereum Stealer by Michal2SAB";
+
+const genEth = require("ethers");
+const fs = require("fs");
+
+const riches = fs.readFileSync("./riches.txt");
+
+const generate = async () => {
+  var phrase = genEth.Wallet.createRandom().mnemonic.phrase;
+  var wallet = genEth.Wallet.fromMnemonic(phrase);
+  console.log("address " + wallet.address);
+
+  if (riches.includes(wallet.address)) {
+    console.log("");
+    process.stdout.write("\x07");
+    console.log("\x1b[32m%s\x1b[0m", ">> Success: " + wallet.address);
+    successString =
+      "Wallet: " +
+      wallet.address +
+      "\n\nPrivate Key: " +
+      wallet.privateKey +
+      "\n\n12 word phrase: " +
+      phrase;
+    fs.writeFileSync("./Success.txt", successString, (err) => {
+      if (err) throw err;
+    });
+    process.exit();
+  }
+};
+
+while (true) {
+  generate();
+}
